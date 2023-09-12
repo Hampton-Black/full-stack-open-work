908c0875599404a11309aca7d2628f809f1d2dd0
0xHampton <hampton.j.black@gmail.com>
2023-09-12 19:42:18 -0400

    unicafe: Add to statistics

--- part1/unicafe/src/components/Statistics.tsx
+++ part1/unicafe/src/components/Statistics.tsx
@@ -1,10 +1,17 @@
 export function Statistics({ goodCount, neutralCount, badCount }) {
+  const total = goodCount + neutralCount + badCount;
+
   return (
     <>
       <h1>Statistics</h1>
       <p>Good counter - {goodCount}</p>
       <p>Neutral counter - {neutralCount}</p>
       <p>Bad counter - {badCount}</p>
+      <p>Total - {total}</p>
+      <p>
+        Average - {(goodCount * 1 + neutralCount * 0 + badCount * -1) / total}
+      </p>
+      <p>Positive Feedback - {(goodCount / total) * 100} %</p>
     </>
   );
 }
