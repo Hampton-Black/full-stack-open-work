a033d1a6587ac66e4dc80743429736b9d3106f42
0xHampton <hampton.j.black@gmail.com>
2023-09-14 17:18:29 -0400

    part 2.1: add Course component

--- part1/courseinfo/src/App.tsx
+++ part1/courseinfo/src/App.tsx
@@ -3,6 +3,12 @@ import "./App.css";
 interface courseParts {
   name: string;
   exercises: number;
+  id: number;
+}
+
+interface course extends courseParts {
+  name: string;
+  parts: courseParts[];
 }
 
 const Header = (props: { courseName: string }) => {
@@ -58,30 +64,42 @@ const Total = (props: { parts: Array<courseParts> }) => {
   );
 };
 
+const Course = (props: { course: course }) => {
+  return (
+    <>
+      <Header courseName={props.course.name} />
+      <Content parts={props.course.parts} />
+      <Total parts={props.course.parts} />
+    </>
+  );
+};
+
 const App = () => {
   const course = {
+    id: 1,
     name: "Half Stack Application Development",
     parts: [
       {
         name: "Fundamentals of React",
         exercises: 10,
+        id: 1,
       },
       {
         name: "Using props to pass data",
         exercises: 7,
+        id: 2,
       },
       {
         name: "State of a Component",
         exercises: 14,
+        id: 3,
       },
     ],
   };
 
   return (
     <div>
-      <Header courseName={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Course course={course} />
     </div>
   );
 };
