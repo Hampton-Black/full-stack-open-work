a75a175021bbafcb5b40691b10b17df11cc00e51
0xHampton <hampton.j.black@gmail.com>
2023-09-14 21:10:13 -0400

    2.8: Add number functionality

--- part2/phonebook/src/App.tsx
+++ part2/phonebook/src/App.tsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
 import "./App.css";
 
+interface person {
+  name: string;
+  number?: string;
+}
+
 function App() {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addName = (event) => {
     event.preventDefault();
     const nameObject = {
       name: newName,
+      number: newNumber,
     };
 
     if (persons.find((person) => newName === person.name)) {
@@ -18,27 +30,39 @@ function App() {
 
     setPersons(persons.concat(nameObject));
     setNewName("");
+    setNewNumber("");
   };
-
-  const handleNameChange = (event) => setNewName(event.target.value);
 
   return (
     <>
-      <h2>Phonebook</h2>
+      <h2 className="header">Phonebook</h2>
 
       <form className="form-container" onSubmit={addName}>
         <div>
-          Name: <input value={newName} onChange={handleNameChange} />
+          Name:{" "}
+          <input
+            value={newName}
+            onChange={(event) => setNewName(event.target.value)}
+          />
+        </div>
+        <div>
+          Number:{" "}
+          <input
+            value={newNumber}
+            onChange={(event) => setNewNumber(event.target.value)}
+          />
         </div>
         <div>
           <button type="submit">Add</button>
         </div>
       </form>
 
-      <h2>Numbers</h2>
+      <h2 className="header">Numbers</h2>
       <ul>
         {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>
+            {person.name} - {person.number}
+          </li>
         ))}
       </ul>
     </>
